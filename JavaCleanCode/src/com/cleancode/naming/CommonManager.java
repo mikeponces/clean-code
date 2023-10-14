38fdba82584b8bf86b0b2079ce7332a0a117e8eb
Build My Wealth <buildmywealthapp@gmail.com>
2023-10-14 16:47:37 +0800

    Naming Matters - Bad example.

---
+++ JavaCleanCode/src/com/cleancode/naming/CommonManager.java
@@ -0,0 +1,12 @@
+package com.cleancode.naming;
+
+import java.util.Map;
+
+public class CommonManager {
+
+	Map<String, String> data;
+
+	public Map<String, String> getData() {
+		return data;
+	}
+}
